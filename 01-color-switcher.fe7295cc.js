239a85e073aa658a4d7f5f8f68a4a6888f2a2dcb
Denys-Zabolotskyi <70952705+Denys-Zabolotskyi@users.noreply.github.com>
2022-08-01 20:26:27 +0000

    Deploying to gh-pages from @ Denys-Zabolotskyi/goit-js-hw-09@7b04346716b49d0f5dc20c83d29ada3c698b9fa2 🚀

--- 01-color-switcher.fe7295cc.js
+++
@@ -1,2 +0,0 @@
-const t={btnStart:document.querySelector("button[data-start]"),btnStop:document.querySelector("button[data-stop]"),body:document.querySelector("body")};t.btnStart.addEventListener("click",(function(t){console.log("start")})),t.btnStop.addEventListener("click",(function(t){console.log("stop")}));
-//# sourceMappingURL=01-color-switcher.fe7295cc.js.map
