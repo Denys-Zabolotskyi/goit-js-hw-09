7b04346716b49d0f5dc20c83d29ada3c698b9fa2
Denys-Zabolotskyi <70952705+Denys-Zabolotskyi@users.noreply.github.com>
2022-08-01 23:26:01 +0300

    task-01-css

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,17 +1,53 @@
+/*********************************Get the links on the elements************************* */
 const refs = {
   btnStart: document.querySelector('button[data-start]'),
   btnStop: document.querySelector('button[data-stop]'),
   body: document.querySelector('body'),
 };
-// console.log(refs.btnStart);
-// console.log(refs.btnStop);
-// console.log(refs.body);
+/******************************Const for class***************************** */
+const DISABLE_CLASS = 'disabled';
+
+/************************************Слухачі подій************************************* */
 refs.btnStart.addEventListener('click', onCklickBtnStart);
 refs.btnStop.addEventListener('click', onCklickBtnStop);
 
+/************************************Timer ID**************************************** */
+let timerId = null;
+
+/******************************Function starts a interval and changes the color****** */
 function onCklickBtnStart(evt) {
+  const currentEl = evt.target;
+  if (currentEl.classList.contains(DISABLE_CLASS)) {
+    return;
+  }
+  addRemoveDisabledClass(currentEl, refs.btnStop, DISABLE_CLASS);
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
   console.log('start');
 }
+/************************************Function stop interval****** */
 function onCklickBtnStop(evt) {
+  if (timerId === null) {
+    return;
+  }
+  const currentEl = evt.target;
+  addRemoveDisabledClass(currentEl, refs.btnStart, DISABLE_CLASS);
+  clearInterval(timerId);
   console.log('stop');
 }
+/**********************************Function adds or removes class******************* */
+
+function addRemoveDisabledClass(currentEl, disabledEl, classAdd) {
+  if (currentEl.classList.contains(classAdd)) {
+    return;
+  }
+  currentEl.classList.add(classAdd);
+  disabledEl.classList.remove(classAdd);
+}
+/***********************************Function generation random color ******************************************** */
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+/******************************************************************************* */
